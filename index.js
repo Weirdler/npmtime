23e2a0bac374d5c1e493e0ddd7792d9465697775
Bren <captainbmq@gmail.com>
2019-06-25 10:56:30 -0400

    playing with npm

---
+++ index.js
@@ -0,0 +1,9 @@
+ const guccinator = require('guccimaticaninator');
+ const bigboi = require('thebigdog')
+ 
+
+guccinator.helloEarth();
+guccinator.greetingHuman();
+bigboi.helloWorld();
+guccinator.lunchtime();
+guccinator.souptime();
